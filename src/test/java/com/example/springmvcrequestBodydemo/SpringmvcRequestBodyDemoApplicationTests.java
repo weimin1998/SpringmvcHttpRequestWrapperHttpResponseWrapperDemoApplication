26c8c752f145ac3ccde934483efe86ddaf0452f2
weimin <2561394933@qq.com>
2024-02-07 19:53:54 +0800

    HttpRequestWrapper HttpResponseWrapper

---
+++ src/test/java/com/example/springmvcrequestBodydemo/SpringmvcRequestBodyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmvcrequestBodydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcRequestBodyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
